005e79a4493220a6de4b3657cdbaf8db92214730
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-06-01 20:09:54 +0200

    add test config

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src', '<rootDir>/__tests__'],
+    testMatch: [
+        '**/__tests__/**/*.test.ts',
+        '**/?(*.)+(spec|test).ts'
+    ],
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/**/*.d.ts',
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: [
+        'text',
+        'lcov',
+        'html'
+    ],
+    moduleFileExtensions: [
+        'ts',
+        'js',
+        'json',
+        'node'
+    ],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    setupFilesAfterEnv: ['<rootDir>/__tests__/setup.ts'],
+    testTimeout: 10000,
+    verbose: true
+}; 
